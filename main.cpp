2809a64021da498945dcf0a29d0c15de0365fbd1
MeetyourMaker <kornelmrozcs@gmail.com>
2024-11-02 14:57:09 +0000

    Vertex Shader init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+﻿#include<iostream>
 #include<glad/glad.h> 
 #include<GLFW/glfw3.h> 
+
+// shaders
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
 
 // prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -10,6 +18,12 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// Vertex Data
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
 
 int main()
 {
@@ -38,12 +52,26 @@ int main()
 		std::cout << "Failed to initialize GLAD\n";
 		return 11;
 	}
+
+	// Shaders: 
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+
 	
 
 
 	// Render Loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//Buffers
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 		processInput(window); // check for input 
 
 
